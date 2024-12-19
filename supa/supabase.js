6cdf46b8cd7b984a9ea96251521ea6e78cbe43c0
laripsilva25 <117595456+laripsilva25@users.noreply.github.com>
2024-12-19 11:10:33 -0300

    trocaDoBD

--- supa/supabase.js
+++ supa/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://swnloxsknktfmqjrgcar.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bmxveHNrbmt0Zm1xanJnY2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3OTg2MjIsImV4cCI6MjA0NTM3NDYyMn0.TdVsqRuhTgYXD5jbF0KFPnQ0TrmtUhWtWTmBKwfipSw';
+const SUPABASE_URL = 'https://bzkjcvxpxqowykqhomub.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6a2pjdnhweHFvd3lrcWhvbXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3ODg1MTIsImV4cCI6MjA0OTM2NDUxMn0.kwfvdvq2j8U9t_M18NfLVYtfW9_rAWCx4e4qrFgSMz0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
