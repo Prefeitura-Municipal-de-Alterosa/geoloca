7fdcd3eb51ee9a77baf0df5ebc2467910f96f7fd
BeatrizMoreira30 <bia30.albano@gmail.com>
2024-12-20 22:35:29 -0300

    ocultandoChave

--- supa/supabase.js
+++ supa/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://bzkjcvxpxqowykqhomub.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6a2pjdnhweHFvd3lrcWhvbXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3ODg1MTIsImV4cCI6MjA0OTM2NDUxMn0.kwfvdvq2j8U9t_M18NfLVYtfW9_rAWCx4e4qrFgSMz0';
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
